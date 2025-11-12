b6d87ab6c9dcef021296057b38aa06e02546dd5d
JonathanCentifanto <jdcentifanto@gmail.com>
2025-11-12 15:30:14 -0600

    Add files via upload
    Updated code with stepsPerInchStrafe value

---
+++ 4Motor_L298N_With_ServoActuator.ino
@@ -0,0 +1,237 @@
+#include <AccelStepper.h>
+#include <Servo.h>
+
+// Use AccelStepper in DRIVER mode (L298N)
+#define MotorInterfaceType 4
+
+AccelStepper FrontRight(MotorInterfaceType, 41,43,45,47); // Yellow (in1), orange(in2), red(in3), brown(in4) respectively
+AccelStepper RearRight(MotorInterfaceType, 40,42,44,46); // Yellow (in1), orange(in2), red(in3), brown(in4) respectively
+
+AccelStepper FrontLeft(MotorInterfaceType, 31,33,35,37); // White (in1), grey(in2), purple(in3), blue(in4) respectively 
+AccelStepper RearLeft(MotorInterfaceType, 30,32,34,36); // White (in1), grey(in2), purple(in3), blue(in4) respectively 
+
+// Servo wiring: Orange/White--PWM, Red--5V, Brown/Black--GND
+Servo FlagServo;
+#define FlagServoPin 10
+
+// Solenoid pins
+#define Solenoid7 2 // presses 7 key
+#define Solenoid3 3 // presses 3 key
+#define Solenoid8 4 // presses 8 key
+#define SolenoidP 5 // presses # key
+
+// Movement and distance defs
+const float inchesPerStep = 0.06; // Inches per step with 3.8 inch wheels; unused at present
+const int stepsPerInch = 16;
+const int stepsPerInchStrafe =  20;
+const int turn90 = 293;
+
+void setup()
+{
+    int speed = 150; // Steps per second, 200 steps per rotation
+    int accel = 1000; // required for thingus to move at all
+
+    FrontRight.setMaxSpeed(speed);
+    RearRight.setMaxSpeed(speed);
+
+    FrontLeft.setMaxSpeed(speed);
+    RearLeft.setMaxSpeed(speed);
+
+    FrontRight.setAcceleration(accel);
+    RearRight.setAcceleration(accel);
+
+    FrontLeft.setAcceleration(accel);
+    RearLeft.setAcceleration(accel);
+
+    FlagServo.attach(FlagServoPin); // Orange wire on servo
+}
+
+void loop() 
+{
+    // MoveFlagServo(180);
+    // delay(1000);
+    // HomeFlagServo();
+    // delay(1000);
+
+    Forward(28);
+    delay(1000);
+    TripleButtonWombo();
+    delay(1000);
+    Reverse(16);
+    delay(1000);
+
+    StrafeRight(24);
+    delay(2000);
+
+    // HomeAllServos();
+}
+
+// STEPPER FUNCTIONS
+void Forward(int inches)
+{
+    FrontRight.move(inches*stepsPerInch);
+    RearRight.move(inches*stepsPerInch);
+
+    FrontLeft.move(inches*stepsPerInch);
+    RearLeft.move(inches*stepsPerInch);
+
+    while (FrontRight.distanceToGo() != 0 || FrontLeft.distanceToGo() != 0 || RearLeft.distanceToGo() != 0 || RearRight.distanceToGo() != 0) 
+    {
+        FrontRight.run();
+        FrontLeft.run();
+        RearLeft.run();
+        RearRight.run();
+    }
+}
+
+void Reverse(int inches)
+{
+    FrontRight.move(-inches*stepsPerInch);
+    RearRight.move(-inches*stepsPerInch);
+
+    FrontLeft.move(-inches*stepsPerInch);
+    RearLeft.move(-inches*stepsPerInch);
+
+    while (FrontRight.distanceToGo() != 0 || FrontLeft.distanceToGo() != 0 || RearLeft.distanceToGo() != 0 || RearRight.distanceToGo() != 0) 
+    {
+        FrontRight.run();
+        FrontLeft.run();
+        RearLeft.run();
+        RearRight.run();
+    }
+}
+
+void StrafeLeft(int inches)
+{
+    FrontRight.move(inches*stepsPerInchStrafe);
+    RearRight.move(inches*stepsPerInchStrafe);
+
+    FrontLeft.move(-inches*stepsPerInchStrafe);
+    RearLeft.move(-inches*stepsPerInchStrafe);
+
+    while (FrontRight.distanceToGo() != 0 || FrontLeft.distanceToGo() != 0 || RearLeft.distanceToGo() != 0 || RearRight.distanceToGo() != 0) 
+    {
+        FrontRight.run();
+        FrontLeft.run();
+        RearLeft.run();
+        RearRight.run();
+    }
+}
+
+void StrafeRight(int inches)
+{
+    float strafeAdjust = (stepsPerInch * (10/9));
+    FrontRight.move(-inches*stepsPerInchStrafe);
+    RearRight.move(-inches*stepsPerInchStrafe);
+
+    FrontLeft.move(inches*stepsPerInchStrafe);
+    RearLeft.move(inches*stepsPerInchStrafe);
+
+    while (FrontRight.distanceToGo() != 0 || FrontLeft.distanceToGo() != 0 || RearLeft.distanceToGo() != 0 || RearRight.distanceToGo() != 0) 
+    {
+        FrontRight.run();
+        FrontLeft.run();
+        RearLeft.run();
+        RearRight.run();
+    }
+}
+
+void RotateLeft(int degrees) // Function to rotate left
+{
+    FrontRight.move(degrees);
+
+    FrontLeft.move(-degrees);
+
+    while (FrontRight.distanceToGo() != 0 || FrontLeft.distanceToGo() != 0) 
+    {
+        FrontRight.run();
+        FrontLeft.run();
+        RearLeft.run();
+        RearRight.run();
+    }
+
+}
+
+void RotateRight(int degrees) //Function to rotate right
+{
+    FrontRight.move(-degrees);
+
+    FrontLeft.move(degrees);
+
+    while (FrontRight.distanceToGo() != 0 || FrontLeft.distanceToGo() != 0) 
+    {
+        FrontRight.run();
+        FrontLeft.run();
+        RearLeft.run();
+        RearRight.run();
+    }
+}
+
+// SPECIFIC FUNCTIONS
+
+void TripleButtonWombo() // Antenna One
+{
+    Forward(1);
+    delay(500);
+    Reverse(1);
+    delay(500);
+
+    Forward(1);
+    delay(500);
+    Reverse(1);
+    delay(500);
+
+    Forward(1);
+    delay(500);
+    Reverse(1); // should land us where we started...
+    delay(500);
+}
+
+void EnterCombination() // Solenoid actions to enter 73738#
+{
+    digitalWrite(Solenoid7, HIGH); // Presses the 7 key on the keypad
+    delay(1000);
+    digitalWrite(Solenoid7, LOW);
+    delay(1000);
+
+    digitalWrite(Solenoid3, HIGH); // Presses the 3 key on the keypad
+    delay(1000);
+    digitalWrite(Solenoid3, LOW);
+    delay(1000);
+
+    digitalWrite(Solenoid7, HIGH); // Presses the 7 key on the keypad
+    delay(1000);
+    digitalWrite(Solenoid7, LOW);
+    delay(1000);
+
+    digitalWrite(Solenoid3, HIGH); // Presses the 3 key on the keypad
+    delay(1000);
+    digitalWrite(Solenoid3, LOW);
+    delay(1000);
+
+    digitalWrite(Solenoid8, HIGH); // Presses the 8 key on the keypad
+    delay(1000);
+    digitalWrite(Solenoid8, LOW);
+    delay(1000);
+
+    digitalWrite(SolenoidP, HIGH); // Presses the # key on the keypad
+    delay(1000);
+    digitalWrite(SolenoidP, LOW);
+    delay(1000);
+}
+
+// SERVO FUNCTIONS
+void MoveFlagServo(int degrees) // Moves servo n degrees
+{
+    FlagServo.write(degrees);
+}
+
+void HomeFlagServo() // Homes flag servo (0 degrees)
+{
+    FlagServo.write(0);
+}
+
+void HomeAllServos() // Homes all servos (0 degrees) add others here
+{
+    FlagServo.write(0);
+}
