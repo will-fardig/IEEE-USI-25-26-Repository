fc93943a4f0e353f9369de05328cd355d1514c50
Will Fardig <willfardig@outlook.com>
2026-03-03 17:45:08 -0600

    Add files via upload
    da new von

--- lab_connect1.py
+++ lab_connect1.py
@@ -11,21 +11,19 @@ from cflib.crazyflie.syncLogger import SyncLogger
 from cflib.positioning.motion_commander import MotionCommander
 from cflib.utils import uri_helper
 
-# Handling the in and out from the Pi
-# Don't forget to install all of ts
-# import RPi.GPIO as GPIO
-# GPIO.setmode(GPIO.BCM)
+import RPi.GPIO as GPIO
+GPIO.setmode(GPIO.BCM)
 
-fromArduino = 1 # pin 1, got from Ard
-toArduino = 2 # pin 2, sent to Ard
+toArduino = 17
+fromArduino = 27
 
-# GPIO.setup(fromArduino, GPIO.IN) # 1 is received from Arduino
-# GPIO.setup(toArduino, GPIO.OUT) # 2 is sent to the Arduino
+GPIO.setup(toArduino, GPIO.OUT)
+GPIO.setup(fromArduino, GPIO.IN)
 
 # URI to the Crazyflie to connect to
 uri = 'radio://0/80/2M'
 logging.basicConfig(level=logging.ERROR)
-position_estimate = [0,0,0]
+position_estimate = [0,0]
 
 DEFAULT_HEIGHT = 0.5 # this is in meters bruv
 deck_attached_event = Event()
@@ -46,14 +44,12 @@ def sparkArduino(scf):
         mc.left(0.75)
         time.sleep(1)
         mc.stop()
-        # GPIO.output(2, GPIO.HIGH)
 
 def log_pos_callback(timestamp, data, logconf):
     global position_estimate
     position_estimate[0] = data['stateEstimate.x']
     position_estimate[1] = data['stateEstimate.y']
-    position_estimate[2] = data['pm.batteryLevel']
-    print(f"X:{position_estimate[0]}, Y:{position_estimate[1]}, Lowkey waste of a var:{position_estimate[2]:2f}%")
+    print(f"X:{position_estimate[0]}, Y:{position_estimate[1]}")
     time.sleep(1)
 
 if __name__ == '__main__':
@@ -63,7 +59,6 @@ if __name__ == '__main__':
         logconf = LogConfig(name="Position", period_in_ms=1000)
         logconf.add_variable('stateEstimate.x', 'float')
         logconf.add_variable('stateEstimate.y', 'float')
-        logconf.add_variable('pm.batteryLevel', 'float')
         scf.cf.log.add_config(logconf)
         logconf.data_received_cb.add_callback(log_pos_callback)
         
@@ -73,13 +68,15 @@ if __name__ == '__main__':
         if not deck_attached_event.wait(timeout=3):
             print('No deck detected dumbass')
             sys.exit(1)
-        
-        # while GPIO.input(1) == 0:
-        #     print("Waiting for Arduino to be done")
-        #     time.sleep(1)
+                
+        while GPIO.input(fromArduino) == 0:
+            print("Waiting for Arduino to tell us to do something")
+            time.sleep(1)
         scf.cf.platform.send_arming_request(True)
         time.sleep(1)
         
         logconf.start()
         sparkArduino(scf)
         logconf.stop()
+        
+        GPIO.output(toArduino, GPIO.HIGH)
